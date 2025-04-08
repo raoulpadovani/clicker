903e62cda4653dbfa673adbd9026a639561d9654
raoul-padovani <raoul.padovani@laplateforme.io>
2025-04-09 01:12:53 +0200

    paliers

--- clicker.js
+++ clicker.js
@@ -1,6 +1,24 @@
 const button = document.querySelector("button");
 const counter = document.getElementById("counter");
 let count = 0;
-button.addEventListener("click", () => {
+button.addEventListener("click", () => 
+{
   count++;
-  counter.textContent = count;});
+  counter.textContent = count;
+  if (count == 10) {
+    document.getElementById("pierre").addEventListener("click", () => {
+      document.body.style.cursor = "url('piochep.png'), auto";
+    });}
+    if (count == 20) {
+      document.getElementById("or").addEventListener("click", () => {
+        document.body.style.cursor = "url('piochej.png'), auto";
+      });
+    
+  }
+});
+
+
+
+
+
+
