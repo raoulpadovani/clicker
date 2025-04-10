903ed19007a69b8b0c5c3c2855d24a4cdd85f63c
raoul-padovani <raoul.padovani@laplateforme.io>
2025-04-10 17:15:18 +0200

    fin du shop , mob , paysage

--- js/clicker.js
+++ js/clicker.js
@@ -4,6 +4,7 @@ const counter = document.getElementById("counter");
 const buttonBlock = document.getElementById("block");
 const buttonFer = document.getElementById("fer");
 const buttonOr = document.getElementById("or");
+
 const buttonDiamant = document.getElementById("diamant");
 const buttonNetherite = document.getElementById("netherite");
 const buttonHachefer = document.getElementById("hachefer");
@@ -11,18 +12,21 @@ const buttonHacheor = document.getElementById("hacheor");
 const buttonHachediamant = document.getElementById("hachediamant");
 const buttonHachenetherite = document.getElementById("hachenetherite");
 
+const buttonEpeefer = document.getElementById("epeefer");
+const buttonEpeeor = document.getElementById("epeeor");
+const buttonEpeediamant = document.getElementById("epeediamant");
+const buttonEpeenetherite = document.getElementById("epeenetherite");
+
 const buttonReset = document.getElementById("reset");
-const buttonPlay = document.getElementById("son"); // 
-const buttonStop = document.getElementById("stop"); // 
+const buttonPlay = document.getElementById("son"); 
+const buttonStop = document.getElementById("stop"); 
 
-let count = parseInt(localStorage.getItem("count")) || 0; // Récupère le score depuis le stockage local ou initialise à 0
-
-
-let lastBlock = localStorage.getItem("lastBlock") || "asset/block/p.png"; // Dernier bloc par défaut
-let lastPickaxe = localStorage.getItem("lastPickaxe") || "asset/item/mp.png"; // Dernière pioche par défaut
-let lastBlockWidth = localStorage.getItem("lastBlockWidth") || "200px"; // Largeur par défaut
-let lastBlockHeight = localStorage.getItem("lastBlockHeight") || "200px"; // Hauteur par défaut
-let lastFond = localStorage.getItem("lastfond") || "asset/paysage/paysagearbre.png"; // Dernier bloc par défaut
+let count = parseInt(localStorage.getItem("count")) || 0; 
+let lastBlock = localStorage.getItem("lastBlock") || "asset/block/p.png"; 
+let lastPickaxe = localStorage.getItem("lastPickaxe") || "asset/item/mp.png"; 
+let lastBlockWidth = localStorage.getItem("lastBlockWidth") || "200px"; 
+let lastBlockHeight = localStorage.getItem("lastBlockHeight") || "200px"; 
+let lastFond = localStorage.getItem("lastFond") || "/clicker/asset/paysage/fond.png";
 
 // Met à jour l'affichage du score au chargement
 counter.textContent = count;
@@ -36,71 +40,80 @@ img.style.height = lastBlockHeight;
 
 // Autoclick actif dès le début
 setInterval(() => {
-  count += 1; // Incrémente le score automatiquement
-  counter.textContent = count; // Met à jour l'affichage
-  localStorage.setItem("count", count); // Enregistre le score dans le stockage local
-}, 1000); // Intervalle de 1 seconde
+  count += 1; 
+  counter.textContent = count; 
+  localStorage.setItem("count", count); 
+}, 1000); 
 
 /* gold click */
 buttonBlock.addEventListener("click", () => {
   count += 1;
   counter.textContent = count;
-  localStorage.setItem("count", count); // Enregistre le score dans le stockage local
-  console.log(count, 'yo');
+  localStorage.setItem("count", count); 
 });
 /** son */
 buttonPlay.addEventListener("click", () => {
   const audio = document.getElementById("audio");
   if (audio.paused) {
     audio.play();
-    buttonPlay.textContent = "Arrêter le son"; // Change le texte du bouton
+    const img = buttonPlay.querySelector("img");
+    img.src = "asset/item/chef.gif";
   } else {
     audio.pause();
-    audio.currentTime = 0; // Remet le son au début
-    buttonPlay.textContent = "Jouer le son"; // Change le texte du bouton
+    audio.currentTime = 0;
+    const img = buttonPlay.querySelector("img");
+    img.src = "asset/item/jukebox.gif"; 
+
   }
 });
 
 /* reset */
 buttonReset.addEventListener("click", () => {
   if (confirm("Êtes-vous sûr de vouloir réinitialiser le jeu ?")) {
-    // Réinitialise toutes les valeurs
-    count = 0; // Réinitialise le score
-    counter.textContent = count; // Met à jour l'affichage du score
-    localStorage.setItem("count", count); // Enregistre le score réinitialisé dans le stockage local
+    count = 0; 
+    counter.textContent = count;
+    localStorage.setItem("count", count); 
 
     // Réinitialise la pioche et le bloc
-    document.body.style.cursor = "url('asset/item/mp.png'), auto"; // Réinitialise la pioche
-    localStorage.setItem("lastPickaxe", "asset/item/mp.png"); // Réinitialise la dernière pioche
+    document.body.style.cursor = "url('asset/item/mp.png'), auto"; 
+    localStorage.setItem("lastPickaxe", "asset/item/mp.png"); 
 
     const img = buttonBlock.querySelector("img");
-    img.src = "asset/block/p.png"; // Réinitialise le bloc
-    img.style.width = "200px"; // Réinitialise la largeur
-    img.style.height = "200px"; // Réinitialise la hauteur
-    localStorage.setItem("lastBlock", "asset/block/p.png"); // Réinitialise le dernier bloc
-    localStorage.setItem("lastBlockWidth", "200px"); // Réinitialise la largeur
-    localStorage.setItem("lastBlockHeight", "200px"); // Réinitialise la hauteur
+    img.src = "asset/block/p.png"; 
+    img.style.width = "200px"; 
+    img.style.height = "200px"; 
+    localStorage.setItem("lastBlock", "asset/block/p.png"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px"); 
+
+    // Réinitialise le fond
+    document.body.style.backgroundImage = "url('asset/paysage/fond.png')";
+    localStorage.setItem("lastFond", "asset/paysage/fond.png");
 
     // Affiche un message de confirmation
     console.log("Le jeu a été réinitialisé.");
   }
 });
-
 /*amélioration*/
 buttonFer.addEventListener("click", () => {
   if (count >= 30) {
     count -= 30;
     counter.textContent = count;
-    localStorage.setItem("count", count); // Enregistre le score dans le stockage local
+    localStorage.setItem("count", count); 
     document.body.style.cursor = "url('asset/item/mi.png'), auto";
-    localStorage.setItem("lastPickaxe", "asset/item/mi.png"); // Sauvegarde la dernière pioche
+    localStorage.setItem("lastPickaxe", "asset/item/mi.png"); 
     const img = buttonBlock.querySelector("img");
     img.src = "asset/block/block fer.png";
     img.style.width = "200px";
     img.style.height = "200px";
-    localStorage.setItem("lastBlock", "asset/block/block fer.png"); // Sauvegarde le dernier bloc
-    localStorage.setItem("lastBlockWidth", "200px"); // Sauvegarde la largeur
-    localStorage.setItem("lastBlockHeight", "200px"); // Sauvegarde la hauteur
+    localStorage.setItem("lastBlock", "asset/block/block fer.png"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px"); 
+    buttonBlock.addEventListener("click", () => {
+      count += 50000;
+      counter.textContent = count;
+      localStorage.setItem("count", count); 
+    });
   }
 });
 
@@ -108,16 +121,21 @@ buttonOr.addEventListener("click", () => {
   if (count >= 50) {
     count -= 50;
     counter.textContent = count;
-    localStorage.setItem("count", count); // Enregistre le score dans le stockage local
+    localStorage.setItem("count", count); 
     document.body.style.cursor = "url('asset/item/min.png'), auto";
-    localStorage.setItem("lastPickaxe", "asset/item/min.png"); // Sauvegarde la dernière pioche
+    localStorage.setItem("lastPickaxe", "asset/item/min.png"); 
     const img = buttonBlock.querySelector("img");
     img.src = "asset/block/block or.png";
     img.style.width = "200px";
     img.style.height = "200px";
-    localStorage.setItem("lastBlock", "asset/block/block or.png"); // Sauvegarde le dernier bloc
-    localStorage.setItem("lastBlockWidth", "200px"); // Sauvegarde la largeur
-    localStorage.setItem("lastBlockHeight", "200px"); // Sauvegarde la hauteur
+    localStorage.setItem("lastBlock", "asset/block/block or.png"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px");
+    buttonBlock.addEventListener("click", () => {
+      count += 4;
+      counter.textContent = count;
+      localStorage.setItem("count", count); 
+    });
   }
 });
 
@@ -125,16 +143,21 @@ buttonDiamant.addEventListener("click", () => {
   if (count >= 80) {
     count -= 80;
     counter.textContent = count;
-    localStorage.setItem("count", count); // Enregistre le score dans le stockage local
+    localStorage.setItem("count", count); 
     document.body.style.cursor = "url('asset/item/mpiochediamant.png'), auto";
-    localStorage.setItem("lastPickaxe", "asset/item/mpiochediamant.png"); // Sauvegarde la dernière pioche
+    localStorage.setItem("lastPickaxe", "asset/item/mpiochediamant.png"); 
     const img = buttonBlock.querySelector("img");
     img.src = "asset/block/block diamant.png";
     img.style.width = "200px";
     img.style.height = "200px";
-    localStorage.setItem("lastBlock", "asset/block/block diamant.png"); // Sauvegarde le dernier bloc
-    localStorage.setItem("lastBlockWidth", "200px"); // Sauvegarde la largeur
-    localStorage.setItem("lastBlockHeight", "200px"); // Sauvegarde la hauteur
+    localStorage.setItem("lastBlock", "asset/block/block diamant.png"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px");
+    buttonBlock.addEventListener("click", () => {
+      count += 8;
+      counter.textContent = count;
+      localStorage.setItem("count", count); 
+    });
   }
 });
 
@@ -142,16 +165,16 @@ buttonNetherite.addEventListener("click", () => {
   if (count >= 110) {
     count -= 110;
     counter.textContent = count;
-    localStorage.setItem("count", count); // Enregistre le score dans le stockage local
+    localStorage.setItem("count", count); 
     document.body.style.cursor = "url('asset/item/mine.png'), auto";
-    localStorage.setItem("lastPickaxe", "asset/item/mine.png"); // Sauvegarde la dernière pioche
+    localStorage.setItem("lastPickaxe", "asset/item/mine.png"); 
     const img = buttonBlock.querySelector("img");
     img.src = "asset/block/block netherite.png";
     img.style.width = "200px";
     img.style.height = "200px";
-    localStorage.setItem("lastBlock", "asset/block/block netherite.png"); // Sauvegarde le dernier bloc
-    localStorage.setItem("lastBlockWidth", "200px"); // Sauvegarde la largeur
-    localStorage.setItem("lastBlockHeight", "200px"); // Sauvegarde la hauteur
+    localStorage.setItem("lastBlock", "asset/block/block netherite.png"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px"); 
   }
 });
 
@@ -162,72 +185,167 @@ buttonHachefer.addEventListener("click", () => {
   if (count >= 220) {
     count -= 220;
     counter.textContent = count;
-
-    // Change le fond en paysagearbre.png
+    
     document.body.style.backgroundImage = "url('asset/paysage/paysagearbre.png')";
-    localStorage.setItem("lastFond", "asset/paysage/paysagearbre.png"); // Sauvegarde le paysage dans le stockage local
-
-    localStorage.setItem("count", count); // Enregistre le score dans le stockage local
+    localStorage.setItem("lastFond", "/clicker/asset/paysage/paysagearbre.png");
+    localStorage.setItem("count", count); 
     document.body.style.cursor = "url('asset/item/hf.png'), auto";
-    localStorage.setItem("lastPickaxe", "asset/item/hf.png"); // Sauvegarde la dernière pioche
-
+    localStorage.setItem("lastPickaxe", "asset/item/hf.png"); 
     const img = buttonBlock.querySelector("img");
     img.src = "asset/block/bouleau.png";
     img.style.width = "200px";
     img.style.height = "200px";
-    localStorage.setItem("lastBlock", "asset/block/bouleau.png"); // Sauvegarde le dernier bloc
-    localStorage.setItem("lastBlockWidth", "200px"); // Sauvegarde la largeur
-    localStorage.setItem("lastBlockHeight", "200px"); // Sauvegarde la hauteur
+    localStorage.setItem("lastBlock", "asset/block/bouleau.png"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px"); 
+    
   }
 });
 
-buttonOr.addEventListener("click", () => {
-  if (count >= 50) {
-    count -= 50;
+buttonHacheor.addEventListener("click", () => {
+  if (count >= 440) {
+    count -= 4400;
+
     counter.textContent = count;
-    localStorage.setItem("count", count); // Enregistre le score dans le stockage local
-    document.body.style.cursor = "url('asset/item/min.png'), auto";
-    localStorage.setItem("lastPickaxe", "asset/item/min.png"); // Sauvegarde la dernière pioche
+    
+    document.body.style.backgroundImage = "url('asset/paysage/paysagearbre.png')";
+    localStorage.setItem("lastFond", "/clicker/asset/paysage/paysagearbre.png");
+    localStorage.setItem("count", count); 
+    document.body.style.cursor = "url('asset/item/ho.png'), auto";
+    localStorage.setItem("lastPickaxe", "asset/item/ho.png"); 
     const img = buttonBlock.querySelector("img");
-    img.src = "asset/block/block or.png";
+    img.src = "asset/block/chene.png";
     img.style.width = "200px";
     img.style.height = "200px";
-    localStorage.setItem("lastBlock", "asset/block/block or.png"); // Sauvegarde le dernier bloc
-    localStorage.setItem("lastBlockWidth", "200px"); // Sauvegarde la largeur
-    localStorage.setItem("lastBlockHeight", "200px"); // Sauvegarde la hauteur
+    localStorage.setItem("lastBlock", "asset/block/chene.png"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px"); 
   }
 });
 
-buttonDiamant.addEventListener("click", () => {
-  if (count >= 80) {
-    count -= 80;
+buttonHachediamant.addEventListener("click", () => {
+  if (count >= 880) {
+    count -= 880;
     counter.textContent = count;
-    localStorage.setItem("count", count); // Enregistre le score dans le stockage local
-    document.body.style.cursor = "url('asset/item/mpiochediamant.png'), auto";
-    localStorage.setItem("lastPickaxe", "asset/item/mpiochediamant.png"); // Sauvegarde la dernière pioche
+    
+    document.body.style.backgroundImage = "url('asset/paysage/paysagearbre.png')";
+    localStorage.setItem("lastFond", "/clicker/asset/paysage/paysagearbre.png");
+    localStorage.setItem("count", count); 
+    document.body.style.cursor = "url('asset/item/hd.png'), auto";
+    localStorage.setItem("lastPickaxe", "asset/item/hd.png"); 
+
     const img = buttonBlock.querySelector("img");
-    img.src = "asset/block/block diamant.png";
+    img.src = "asset/block/arbre.png";
     img.style.width = "200px";
     img.style.height = "200px";
-    localStorage.setItem("lastBlock", "asset/block/block diamant.png"); // Sauvegarde le dernier bloc
-    localStorage.setItem("lastBlockWidth", "200px"); // Sauvegarde la largeur
-    localStorage.setItem("lastBlockHeight", "200px"); // Sauvegarde la hauteur
+    localStorage.setItem("lastBlock", "asset/block/arbre.png"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px"); 
+  }
+});
+buttonHachenetherite.addEventListener("click", () => {
+  if (count >= 1760) {
+    count -= 1760;
+    counter.textContent = count;
+    
+    document.body.style.backgroundImage = "url('asset/paysage/paysagearbre.png')";
+    localStorage.setItem("lastFond", "/clicker/asset/paysage/paysagearbre.png");
+    localStorage.setItem("count", count); 
+    document.body.style.cursor = "url('asset/item/hn.png'), auto";
+    localStorage.setItem("lastPickaxe", "asset/item/hn.png"); 
+
+    const img = buttonBlock.querySelector("img");
+    img.src = "asset/block/pale.png";
+    img.style.width = "200px";
+    img.style.height = "200px";
+    localStorage.setItem("lastBlock", "asset/block/pale.png"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px"); 
   }
 });
 
-buttonNetherite.addEventListener("click", () => {
-  if (count >= 110) {
-    count -= 110;
+buttonEpeefer.addEventListener("click", () => {
+  if (count >= 3520) {
+    count -= 3520;
     counter.textContent = count;
-    localStorage.setItem("count", count); // Enregistre le score dans le stockage local
-    document.body.style.cursor = "url('asset/item/mine.png'), auto";
-    localStorage.setItem("lastPickaxe", "asset/item/mine.png"); // Sauvegarde la dernière pioche
+    
+    document.body.style.backgroundImage = "url('asset/paysage/combat.png')";
+    localStorage.setItem("lastFond", "/clicker/asset/paysage/combat.png");
+    localStorage.setItem("count", count); 
+    document.body.style.cursor = "url('asset/item/sf.png'), auto";
+    localStorage.setItem("lastPickaxe", "asset/item/sf.png"); 
     const img = buttonBlock.querySelector("img");
-    img.src = "asset/block/block netherite.png";
+    img.src = "asset/block/v.gif";
     img.style.width = "200px";
     img.style.height = "200px";
-    localStorage.setItem("lastBlock", "asset/block/block netherite.png"); // Sauvegarde le dernier bloc
-    localStorage.setItem("lastBlockWidth", "200px"); // Sauvegarde la largeur
-    localStorage.setItem("lastBlockHeight", "200px"); // Sauvegarde la hauteur
+    localStorage.setItem("lastBlock", "asset/block/v.gif"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px"); 
+    
   }
 });
+
+buttonEpeeor.addEventListener("click", () => {
+  if (count >= 7040) {
+    count -= 7040;
+
+    counter.textContent = count;
+    
+    document.body.style.backgroundImage = "url('asset/paysage/combat.png')";
+    localStorage.setItem("lastFond", "/clicker/asset/paysage/combat.png");
+    localStorage.setItem("count", count); 
+    document.body.style.cursor = "url('asset/item/so.png'), auto";
+    localStorage.setItem("lastPickaxe", "asset/item/so.png"); 
+    const img = buttonBlock.querySelector("img");
+    img.src = "asset/block/blaze.gif";
+    img.style.width = "300px";
+    img.style.height = "300px";
+    localStorage.setItem("lastBlock", "asset/block/blaze.gif"); 
+    localStorage.setItem("lastBlockWidth", "300px"); 
+    localStorage.setItem("lastBlockHeight", "300px"); 
+  }
+});
+
+buttonEpeediamant.addEventListener("click", () => {
+  if (count >= 14080) {
+    count -= 14080;
+    counter.textContent = count;
+    
+    document.body.style.backgroundImage = "url('asset/paysage/combat.png')";
+    localStorage.setItem("lastFond", "/clicker/asset/paysage/combat.png");
+    localStorage.setItem("count", count); 
+    document.body.style.cursor = "url('asset/item/sd.png'), auto";
+    localStorage.setItem("lastPickaxe", "asset/item/sd.png"); 
+
+    const img = buttonBlock.querySelector("img");
+    img.src = "asset/block/g.gif";
+    img.style.width = "200px";
+    img.style.height = "200px";
+    localStorage.setItem("lastBlock", "asset/block/g.gif"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px"); 
+  }
+});
+
+buttonEpeenetherite.addEventListener("click", () => {
+  if (count >= 28160) {
+    count -= 28160;
+    counter.textContent = count;
+    
+    document.body.style.backgroundImage = "url('asset/paysage/combat.png')";
+    localStorage.setItem("lastFond", "/clicker/asset/paysage/combat.png");
+    localStorage.setItem("count", count); 
+    document.body.style.cursor = "url('asset/item/sn.png'), auto";
+    localStorage.setItem("lastPickaxe", "asset/item/sn.png"); 
+
+    const img = buttonBlock.querySelector("img");
+    img.src = "asset/block/dragon.gif";
+    img.style.width = "200px";
+    img.style.height = "200px";
+    localStorage.setItem("lastBlock", "asset/block/dragon.gif"); 
+    localStorage.setItem("lastBlockWidth", "200px"); 
+    localStorage.setItem("lastBlockHeight", "200px"); 
+  }
+});
+
+
